feae4be39492ea3786543be499eb25c0a88820bb
Raghu Venkatesh <78893470+Raghu-Raman@users.noreply.github.com>
2026-04-11 17:37:30 -0500

    Update solution for la-hadamard-product

--- study-plans/math-linear-algebra/la-hadamard-product/la-hadamard-product.py
+++ study-plans/math-linear-algebra/la-hadamard-product/la-hadamard-product.py
@@ -5,9 +5,4 @@ def hadamard_product(A, B):
     Returns: ndarray, the element-wise product A * B.
     """
     
-    return_array = [[0 for i in range(len(A[0]))] for j in range(len(A))]
-    for i in range(len(A)):
-        for j in range(len(A[0])):
-            return_array[i][j] = A[i][j] * B[i][j]
-    return_array = np.asarray(return_array,dtype=float)
-    return return_array
+    return np.array(A) * np.array(B)
