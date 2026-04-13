6c88830d82ca0a7aa7bf276a2acc6b994cf3e85b
Raghu Venkatesh <78893470+Raghu-Raman@users.noreply.github.com>
2026-04-13 14:00:42 -0500

    Update solution for la-vector-projection

--- study-plans/math-linear-algebra/la-vector-projection/la-vector-projection.py
+++ study-plans/math-linear-algebra/la-vector-projection/la-vector-projection.py
@@ -4,8 +4,8 @@ def vector_projection(u, v):
     """
     Returns: float64 array, the projection of u onto v.
     """
-    u = np.asarray(u)
-    v = np.asarray(v)
+    u = np.asarray(u,dtype =float)
+    v = np.asarray(v,dtype = float)
     num = np.dot(v,u)
     den = np.dot(v,v)
     res = num/den * v
