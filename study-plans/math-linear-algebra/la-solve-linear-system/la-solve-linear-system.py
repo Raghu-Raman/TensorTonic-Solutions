e266ec41448c67ae2170754a9bd7a603291c27ec
Raghu Venkatesh <78893470+Raghu-Raman@users.noreply.github.com>
2026-04-27 13:26:20 -0500

    Update solution for la-solve-linear-system

--- study-plans/math-linear-algebra/la-solve-linear-system/la-solve-linear-system.py
+++ study-plans/math-linear-algebra/la-solve-linear-system/la-solve-linear-system.py
@@ -4,5 +4,11 @@ def solve_linear_system(A, b):
     """
     Returns: float64 array, the solution x to A @ x = b.
     """
-    return np.linalg.solve(np.array(A, dtype=float), np.array(b, dtype=float))
-
+    A = np.array(A,dtype = float)
+    b = np.array(b,dtype = float)
+    
+    if A.shape[0] == A.shape[1]:
+        x = np.linalg.solve(A,b)
+    else:
+        x, residual, rank, sv = np.linalg.lstsq(A,b, rcond=None)
+    return x
