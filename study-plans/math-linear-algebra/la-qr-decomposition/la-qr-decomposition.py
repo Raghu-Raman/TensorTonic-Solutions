73c4cdad1f54084c049b484dc8c80adf7e686b10
Raghu Venkatesh <78893470+Raghu-Raman@users.noreply.github.com>
2026-07-27 22:10:55 -0500

    Solve la-qr-decomposition

--- study-plans/math-linear-algebra/la-qr-decomposition/la-qr-decomposition.py
+++ study-plans/math-linear-algebra/la-qr-decomposition/la-qr-decomposition.py
@@ -4,19 +4,19 @@ def qr_decompose(A):
     """
     Returns: tuple (Q, R) where A = Q @ R.
     """
-    A = np.array(A,dtype=float)
-    m,n = A.shape
-    Q = np.zeros((m,n))
-    R = np.zeros((n,n))
-    for k in range(n):
-        v = A[:,k].copy()
-        for j in range(k):
-            R[j,k] = Q[:,j] @ A[:,k]
-            v -= R[j,k] * Q[:, j]
-        R[k,k] = np.linalg.norm(v)
-        Q[:,k] = v/R[k,k]
-    for k in range(n):
-        if R[k,k] < 0:
-            R[k, :] *= -1
-            Q[:, k] *= -1
-    return Q,R
+    A =  np.array(A, dtype=float)
+    At = A.T
+    return_array = []
+    temp = None
+    for i in range(len(At)):
+        temp = At[i].copy()
+        for j in return_array:
+           proj = np.dot(At[i], j) / np.dot(j, j) * j
+           temp = temp - proj
+        temp = temp/np.linalg.norm(temp)
+        return_array.append(temp)
+    Q = np.array(return_array).T
+    Qt = Q.T
+    R = np.matmul(Qt,A)
+    return (Q,R)
+                      
